51437331fb6b15234623e7a084a7bdfa04306d79
Daniel Opatich <daniel.opatich@gmail.com>
2015-11-12 17:35:44 -0600

    init commit, yo

---
+++ app.js
@@ -0,0 +1,36 @@
+var App = {};
+App.Models = {};
+App.Views = {};
+App.Collections = {};
+
+App.Router = Backbone.Router.extend({
+  routes: {
+    '':'',
+    '':''
+  },
+
+  blank: function() {
+
+
+  },
+
+  blank: function() {
+
+
+  },
+});
+
+function build(){
+  var view = new View({
+    collection: collection
+  });
+  collection.fetch().then(function(){
+    $('.').html(view.render().el)
+  }.bind(this));
+}
+
+
+$('document').ready(function(){
+  App.router = new App.Router();
+  Backbone.history.start();
+})
